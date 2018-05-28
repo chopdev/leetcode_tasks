a95de45a1d6e9a3cae47f1c43627fdad5a98226f
madman1 <taras.plavin@gmail.com>
2018-05-28 22:04:19 +0300

    add description

--- string/67_add_binary/Solution.java
+++ string/67_add_binary/Solution.java
@@ -3,7 +3,15 @@ package chopdev.add_binary_67;
 import java.util.*;
 import java.lang.*;
 
-
+/*
+67. Add Binary
+https://leetcode.com/problems/add-binary/
+Given two binary strings, return their sum (also a binary string).
+For example,
+a = "11"
+b = "1"
+Return "100".
+*/
 public class Solution {
 
   public String addBinary(String a, String b) {
