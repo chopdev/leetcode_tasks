4f68d048486c8f426f1181aeb3cff8761a791f0c
madman1 <taras.plavin@gmail.com>
2018-05-30 23:31:47 +0300

    add description

--- string/39_say_sequence/Solution.java
+++ string/39_say_sequence/Solution.java
@@ -3,6 +3,18 @@ package chopdev.say_sequence_38;
 import java.util.*;
 import java.lang.*;
 
+/*
+38. Count and Say
+https://leetcode.com/problems/count-and-say/
+The count-and-say sequence is the sequence of integers beginning as follows:
+1, 11, 21, 1211, 111221, ...
+1 is read off as "one 1" or 11.
+11 is read off as "two 1s" or 21.
+21 is read off as "one 2, then one 1" or 1211.
+Given an integer n, generate the nth sequence.
+
+Note: The sequence of integers will be represented as a string.
+*/
 public class Solution {
 
   public String countAndSay(int n) {
@@ -35,5 +47,4 @@ public class Solution {
 
     return number;
   }
-
 }
