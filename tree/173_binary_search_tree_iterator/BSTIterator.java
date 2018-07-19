b17c9f2f86dcefa70938399060c97c923d7f3f4d
chopdev <taras.plavin@gmail.com>
2018-07-19 17:48:03 +0300

    add description and picture of example tree

--- tree/173_binary_search_tree_iterator/BSTIterator.java
+++ tree/173_binary_search_tree_iterator/BSTIterator.java
@@ -1,9 +1,32 @@
 import java.util.Stack;
 
+/**
+ 173. Binary Search Tree Iterator
+ https://leetcode.com/problems/binary-search-tree-iterator/
+ Implement an iterator over a binary search tree (BST). Your iterator will be initialized with the root node of a BST.
+ Calling next() will return the next smallest number in the BST.
+ Note: next() and hasNext() should run in average O(1) time and uses O(h) memory, where h is the height of the tree.
+ */
+
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+/**
+ * Your BSTIterator will be called like this:
+ * BSTIterator i = new BSTIterator(root);
+ * while (i.hasNext()) v[f()] = i.next();
+ */
+
 public class BSTIterator {
 
     private Stack<TreeNode> stack = new Stack<>();
-
 
     public BSTIterator(TreeNode root) {
         pushSmaller(root);
@@ -16,12 +39,14 @@ public class BSTIterator {
 
     /** @return the next smallest number */
     public int next() {
+        if(!hasNext()) return 0;
         TreeNode curr = stack.pop();
-        pushSmaller(curr.right);
+        pushSmaller(curr.right); // push to stack right element and all left child elements of this right element
 
         return curr.val;
     }
 
+    /** add to stack all smaller elements (left branch) */
     private void pushSmaller(TreeNode root) {
         TreeNode curr = root;
 
