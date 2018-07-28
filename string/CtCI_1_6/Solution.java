e0d3340b2b8399a0274146b79e156f4698cf81dc
chopdev <taras.plavin@gmail.com>
2018-07-28 15:10:39 +0300

    add description

--- string/CtCI_1_6/Solution.java
+++ string/CtCI_1_6/Solution.java
@@ -1,5 +1,13 @@
+
+//*
+// String Compression: Implement a method to perform basic string compression using the counts
+//of repeated characters. For example, the string aabcccccaaa would become a2b1c5a3. If the
+//"compressed" string would not become smaller than the original string, your method should return
+//the original string. You can assume the string has only uppercase and lowercase letters (a - z).
+// /
 public class Solution {
 
+    // Mine solution Time: O(N), Space complexity: O(N)
     public String shorthand(String str) {
         if(str == null || str.length() == 0) return str;
 
