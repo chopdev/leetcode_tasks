7ab32798d758b3938e84a65eea3d6c265d0b9b48
chopdev <taras.plavin@gmail.com>
2018-08-01 18:40:41 +0300

    add not working solution for 443

--- string/CtCI_1_6/Main.java
+++ string/CtCI_1_6/Main.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -17,5 +19,23 @@ public class Main {
         System.out.println(t6);
         System.out.println(t7);
 
+        char[] ch0 = new char[] { 'a', 'b', 'c' };
+        char[] ch1 = new char[] { 'a', 'a', 'a', 'b'};
+        char[] ch2 = new char[] { 'a', 'a', 'a', 'b', 'b', 'b'};
+        char[] ch3 = new char[] { 'a', 'b', 'b', 'a'};
+        char[] ch4 = new char[] { 'a', 'a', 'a', 'a', 'a', 'a','a', 'a', 'a'};
+        char[] ch5 = new char[] { 'a', 'a', 'a', 'b', 'b', 'b', 'c'};
+        s.compress(ch0);
+        s.compress(ch1);
+        s.compress(ch2);
+        s.compress(ch3);
+        s.compress(ch4);
+        s.compress(ch5);
+        System.out.println(Arrays.toString(ch0));
+        System.out.println(Arrays.toString(ch1));
+        System.out.println(Arrays.toString(ch2));
+        System.out.println(Arrays.toString(ch3));
+        System.out.println(Arrays.toString(ch4));
+        System.out.println(Arrays.toString(ch5));
     }
 }
