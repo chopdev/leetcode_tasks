bd5c3a9f293d48e625e6c85fe29dd59116a2a5ff
chopdev <taras.plavin@gmail.com>
2018-08-06 14:26:28 +0300

    1

--- linked_list/86_partition_list_2_4_CtCI/Main.java
+++ linked_list/86_partition_list_2_4_CtCI/Main.java
@@ -1,6 +1,27 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        ListNode t1 = new ListNode(1);
+        ListNode t2 = new ListNode(4);
+        ListNode t3 = new ListNode(3);
+        ListNode t4 = new ListNode(2);
+        ListNode t5 = new ListNode(5);
+        ListNode t6 = new ListNode(2);
+
+        t1.next = t2;
+        t2.next = t3;
+        t3.next = t4;
+        t4.next = t5;
+        t5.next = t6;
+
+        Solution2 s = new Solution2();
+        ListNode res = s.partition(t1, 3);
+
+        ListNode curr = res;
+        while (curr != null){
+            System.out.print(curr.val + " ");
+            curr = curr.next;
+        }
+    }
     }
 }
