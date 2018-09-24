c729903bb3e3cadd2fff8ba02ba4542fd0c483d6
chopdev <taras.plavin@gmail.com>
2018-09-25 00:21:43 +0300

    remove redundant part

--- bitsManipulation/CtCI_5_4_next_number/Main.java
+++ bitsManipulation/CtCI_5_4_next_number/Main.java
@@ -5,5 +5,9 @@ public class Main {
         int res1 = s.getNextLargest(0b00100100);
         int res2 = s.getNextLargest(0b00001110);
         int res3 = s.getNextLargest(0b00001111);
+
+
+        //// 0100 1110 -> bigger is 0101 0111
+        /// 0001 0011 -> smaller is 0000 1011
     }
 }
--- bitsManipulation/CtCI_5_4_next_number/Solution.java
+++ bitsManipulation/CtCI_5_4_next_number/Solution.java
@@ -3,28 +3,24 @@
  have the same number of 1 bits in their binary representation.
  */
 public class Solution {
+
+    // Mine solution, time O(1)
+    // solution in the book is harder, but can be applied to both larger and smaller
+    // 0100 1110 -> 0101 0111
     public int getNextLargest(int numb) {
         if(numb == Integer.MAX_VALUE || numb == 0) return 0;
 
         int smBit = getSmallestBit(numb);
+        int mask = 1 << smBit;
 
-        if(((numb >> smBit + 1) & 1) == 0) { // if next bit to smallest is 0
-            int mask = ~(1 << smBit);
-            numb &= mask; // clear smallest bit
-            mask = 1 << (smBit + 1);
-            numb |= mask; // set next to smallest bit to 1
-        }
-        else {
-            int mask = 1 << smBit;
-            int checkNumber = Integer.MAX_VALUE - numb;
-            if(checkNumber <= mask)
-                return 0; // number of type 1111 000 , where mask == 0001 000 , we can't get next largest with the same number of bits
+        int checkNumber = Integer.MAX_VALUE - numb;
+        if(checkNumber <= mask)
+            return 0; // number of type 1111 000 , where mask == 0001 000 , we can't get next largest with the same number of bits
 
-            numb = numb + mask;  //   1000 1110 + 0000 0010 = 1001 0000
-            int newSmallest = getSmallestBit(numb);
-            mask = (1 << (newSmallest - smBit)) - 1; // 0000 0011
-            numb |= mask;
-        }
+        numb = numb + mask;  //   1000 1110 + 0000 0010 = 1001 0000
+        int newSmallest = getSmallestBit(numb);
+        mask = (1 << (newSmallest - smBit - 1)) - 1; // 0000 0011
+        numb |= mask;
 
         return numb;
     }
