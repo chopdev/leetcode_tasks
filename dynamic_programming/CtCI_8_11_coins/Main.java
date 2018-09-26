3089f3764b5759142cab944f25b56985a457b708
chopdev <taras.plavin@gmail.com>
2018-09-26 23:40:29 +0300

    .

--- dynamic_programming/CtCI_8_11_coins/Main.java
+++ dynamic_programming/CtCI_8_11_coins/Main.java
@@ -4,7 +4,7 @@ public class Main {
 	    Solution s = new Solution();
 
         int res1 = s.getNumberOfExchanges(100);
-        int res2 = s.makeChange(100);
+        int res2 = s.getNumberOfExchanges(10);
 
         System.out.println(res1);
         System.out.println(res2);
