3e265303e7a8b6021759f677fb5c7349eb1622dc
chopdev <taras.plavin@gmail.com>
2018-10-29 18:17:17 +0200

    add Solution for 162 - peak element

---
+++ array/162_find_peak_element/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+	    Solution s = new Solution();
+        int res4 = s.findPeakElement(new int[] {4, 3, 2, 1});
+        int res3 = s.findPeakElement(new int[] {2, 1});
+        int res = s.findPeakElement(new int[] {1,2,1,3,5,6,4});
+        int res2 = s.findPeakElement(new int[] {1,2,3,1});
+    }
+}
