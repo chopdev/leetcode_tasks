b35c6889d76bd69db61307e716f27f04fa74429d
chopdev <taras.plavin@gmail.com>
2018-10-30 16:27:30 +0200

    add one more solution for 162

--- array/162_find_peak_element/Solution.java
+++ array/162_find_peak_element/Solution.java
@@ -67,4 +67,19 @@ public class Solution {
         }
         return l;
     }
+	
+	
+	// not mine solution
+	// https://leetcode.com/articles/find-peak-element/
+	 public int findPeakElement3333(int[] nums) {
+        return search(nums, 0, nums.length - 1);
+    }
+    public int search(int[] nums, int l, int r) {
+        if (l == r)
+            return l;
+        int mid = (l + r) / 2;
+        if (nums[mid] > nums[mid + 1])
+            return search(nums, l, mid);
+        return search(nums, mid + 1, r);
+    }
 }
