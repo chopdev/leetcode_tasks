868fe06cbbfdc9484b335073c00d6d6ec3ab52f5
chopdev <taras.plavin@gmail.com>
2018-11-01 21:30:23 +0200

    add complete solution for 289

--- array/289_game_of_life/Main.java
+++ array/289_game_of_life/Main.java
@@ -1,6 +1,19 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    Solution sol = new Solution();
+	    int[][] arr = new int[4][3];
+        arr[0] = new int[] {0,1,0};
+        arr[1] = new int[] {0,0,1};
+        arr[2] = new int[] {1,1,1};
+        arr[3] = new int[] {0,0,0};
+	    sol.gameOfLife(arr);
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[0].length; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
--- array/289_game_of_life/Solution.java
+++ array/289_game_of_life/Solution.java
@@ -39,36 +39,92 @@ import java.util.ArrayList;
  */
 public class Solution {
 
+    // Mine solution
+    // O(M*N) - time, O(1) space
+    // We go through each cell and check if it going to be alive on the next step, by checking neighbor cells
+    // If cell was dead, and will become live, set it to -1
+    // If cell is alive and will continue to be alive, set it to 2
+    // On the second go, just switch -1 and 2 to 1, all other to 0.
     public void gameOfLife(int[][] board) {
         if(board == null) return;
 
-        ArrayList<Point> alive = new ArrayList<>();
-
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[0].length; j++) {
-                if(isAlive(board, i, j)) alive.add(new Point(i, j));
+                if(willAlive(board, i, j)){
+                    boolean isAliveNow = board[i][j] > 0;
+                    if(isAliveNow) board[i][j] = 2;
+                    else board[i][j] = -1;
+                }
             }
         }
 
-        
+        for (int i = 0; i < board.length; i++)
+            for (int j = 0; j < board[0].length; j++)
+                if(board[i][j] == -1 || board[i][j] == 2)
+                    board[i][j] = 1;
+                else
+                    board[i][j] = 0;
     }
 
-    private boolean isAlive(int[][] board, int i, int j) {
-        boolean isAliveNow = board[i][j] == 1;
+    private boolean willAlive(int[][] board, int i, int j) {
+        boolean isAliveNow = board[i][j] > 0;
         int aliveNeighbors = 0;
 
-        if(board.length > i + 1 && board[i+1][j] == 1) aliveNeighbors ++;
-        if(i - 1 >= 0 && board[i-1][j] == 1) aliveNeighbors++;
-        if(j - 1 >= 0 && board[i][j-1] == 1) aliveNeighbors++;
-        if(j + 1 < board[0].length && board[i][j+1] == 1) aliveNeighbors++;
+        if(board.length > i + 1 && board[i+1][j] > 0) aliveNeighbors ++;
+        if(i - 1 >= 0 && board[i-1][j] > 0) aliveNeighbors++;
+        if(j - 1 >= 0 && board[i][j-1] > 0) aliveNeighbors++;
+        if(j + 1 < board[0].length && board[i][j+1] > 0) aliveNeighbors++;
 
-        if(board.length > i + 1 && j - 1 >= 0 && board[i+1][j-1] == 1) aliveNeighbors++;
-        if(board.length > i + 1 && j + 1 < board[0].length && board[i+1][j+1] == 1) aliveNeighbors++;
-        if(i - 1 >= 0 && j - 1 >= 0 && board[i-1][j-1] == 1) aliveNeighbors++;
-        if(i - 1 >= 0 && j + 1 < board[0].length && board[i-1][j+1] == 1) aliveNeighbors++;
+        if(board.length > i + 1 && j - 1 >= 0 && board[i+1][j-1] > 0) aliveNeighbors++;
+        if(board.length > i + 1 && j + 1 < board[0].length && board[i+1][j+1] > 0) aliveNeighbors++;
+        if(i - 1 >= 0 && j - 1 >= 0 && board[i-1][j-1] > 0) aliveNeighbors++;
+        if(i - 1 >= 0 && j + 1 < board[0].length && board[i-1][j+1] > 0) aliveNeighbors++;
 
         if(isAliveNow && aliveNeighbors == 2 || aliveNeighbors == 3) return true;
         if (!isAliveNow && aliveNeighbors == 3) return true;
         return false;
+    }
+
+
+
+    // Not mine clever solution
+    // https://leetcode.com/problems/game-of-life/discuss/73223/Easiest-JAVA-solution-with-explanation
+    // They do not check each neighbour cell manually, but use loop with Math.max, min
+    //+ switch to 3 and 2 that makes checks pretty
+    public void gameOfLife2222(int[][] board) {
+        if (board == null || board.length == 0) return;
+        int m = board.length, n = board[0].length;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                int lives = liveNeighbors(board, m, n, i, j);
+
+                // In the beginning, every 2nd bit is 0;
+                // So we only need to care about when will the 2nd bit become 1.
+                if (board[i][j] == 1 && lives >= 2 && lives <= 3) {
+                    board[i][j] = 3; // Make the 2nd bit 1: 01 ---> 11
+                }
+                if (board[i][j] == 0 && lives == 3) {
+                    board[i][j] = 2; // Make the 2nd bit 1: 00 ---> 10
+                }
+            }
+        }
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                board[i][j] >>= 1;  // Get the 2nd state.
+            }
+        }
+    }
+
+    public int liveNeighbors(int[][] board, int m, int n, int i, int j) {
+        int lives = 0;
+        for (int x = Math.max(i - 1, 0); x <= Math.min(i + 1, m - 1); x++) {
+            for (int y = Math.max(j - 1, 0); y <= Math.min(j + 1, n - 1); y++) {
+                lives += board[x][y] & 1; // will give one if number is 3 (11) or 1 (01)
+            }
+        }
+        lives -= board[i][j] & 1; // remove cell it self, if it was 1
+        return lives;
     }
 }
