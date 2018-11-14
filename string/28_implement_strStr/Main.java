77480e2a7ec33cb387bcbda84bd704f1742a22af
chopdev <taras.plavin@gmail.com>
2018-11-14 22:35:49 +0200

    add solition for 28 implement strStr

---
+++ string/28_implement_strStr/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+	    Solution sol = new Solution();
+	    int ind = sol.strStr("hello", "ll");
+        int ind2 = sol.strStr("", "");
+    }
+}
