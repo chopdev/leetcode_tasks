1002300b7618160e4f582c241749468d3bd03701
chopdev <taras.plavin@gmail.com>
2018-11-19 22:56:38 +0200

    add cool Solution for 5 - longest palindromic substring

--- dynamic_programming/5_longest_palindromic_substring/Solution.java
+++ dynamic_programming/5_longest_palindromic_substring/Solution.java
@@ -88,4 +88,32 @@ public class Solution {
 
         return max;
     }
+
+    // not mine solution
+    // https://leetcode.com/problems/longest-palindromic-substring/solution/
+    // solution 4
+    //Time complexity : O(n^2), space O(1)
+    public String longestPalindrome4444(String s) {
+        if (s == null || s.length() < 1) return "";
+        int start = 0, end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = expandAroundCenter(s, i, i);
+            int len2 = expandAroundCenter(s, i, i + 1);
+            int len = Math.max(len1, len2);
+            if (len > end - start) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+        return s.substring(start, end + 1);
+    }
+
+    private int expandAroundCenter(String s, int left, int right) {
+        int L = left, R = right;
+        while (L >= 0 && R < s.length() && s.charAt(L) == s.charAt(R)) {
+            L--;
+            R++;
+        }
+        return R - L - 1;
+    }
 }
