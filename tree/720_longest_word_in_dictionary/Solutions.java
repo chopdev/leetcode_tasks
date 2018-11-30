97a344490ddebf7a84263dc43266ef4fc6704f07
chopdev <taras.plavin@gmail.com>
2018-11-30 18:30:26 +0200

    add Solutions for 720 longest word in dictionary

--- tree/720_longest_word_in_dictionary/Solutions.java
+++ tree/720_longest_word_in_dictionary/Solutions.java
@@ -1,3 +1,8 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  https://leetcode.com/problems/longest-word-in-dictionary/description/
  720. Longest Word in Dictionary
@@ -26,8 +31,13 @@
  The length of words will be in the range [1, 1000].
  The length of words[i] will be in the range [1, 30].
 
+
+ SOLUTIONS
+ https://leetcode.com/articles/longest-word-in-dictionary/
  **/
 public class Solutions {
+    // My almost working solution
+    // Returns correct value but not always  in lexicographical order
     public String longestWord(String[] words) {
         Trie trie = new Trie();
         for (String word : words) {
@@ -35,5 +45,67 @@ public class Solutions {
         }
 
         return trie.getLongest();
+    }
+
+
+    // My solution completely
+    // Time O(N^2) if we not take into account string operations
+    // Space O(N)
+    // Idea is similar to 300 Longest increasing subsecuence - check it on habr
+    // We are sorting array and for each element check if there is previous element that is prefix of current
+    // and DP length of it > 0
+    // initially words with length 1 has DP 1
+    public String longestWord2222(String[] words) {
+        if (words == null) return null;
+        Arrays.sort(words);
+
+        int[] dp = new int[words.length];
+        for (int i = 0; i < words.length; i++) {
+            if (words[i].length() == 1) dp[i] = 1;
+        }
+
+        for (int i = 1; i < words.length; i++) {
+            if (words[i].length() <= 1) continue;
+            String prefix = words[i].substring(0, words[i].length() - 1);
+            for (int j = 0; j < i; j++) {
+                if (dp[j] > dp[i] && prefix.equals(words[j]))
+                    dp[i] = dp[j] + 1;
+            }
+        }
+
+        int max = 0;
+        int index = -1;
+        for (int i = 0; i < dp.length; i++) {
+            if(dp[i] > max) {
+                max = dp[i];
+                index = i;
+            }
+        }
+
+        return index >= 0 ? words[index] : "";
+    }
+
+
+    // Not mine
+    // Cool solution
+    // pretty easy: For each word, check if all prefixes word[:k] are present. We can use a Set structure to check this quickly.
+    public String longestWord3333(String[] words) {
+        String ans = "";
+        Set<String> wordset = new HashSet();
+        for (String word: words) wordset.add(word);
+        for (String word: words) {
+            if (word.length() > ans.length() ||
+                    word.length() == ans.length() && word.compareTo(ans) < 0) {
+                boolean good = true;
+                for (int k = 1; k < word.length(); ++k) {
+                    if (!wordset.contains(word.substring(0, k))) {
+                        good = false;
+                        break;
+                    }
+                }
+                if (good) ans = word;
+            }
+        }
+        return ans;
     }
 }
