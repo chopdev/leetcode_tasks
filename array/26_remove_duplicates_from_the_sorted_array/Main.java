6a87b14f5b22d813bc6c03f9017e989cdab46ea8
chopdev <taras.plavin@gmail.com>
2018-12-01 15:07:07 +0200

    add solution for 26 remove duplicates from sorted array

---
+++ array/26_remove_duplicates_from_the_sorted_array/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+	    Solution sol = new Solution();
+
+	    int[] arr1 = new int[] {1,1,2};
+        int[] arr2 = new int[] {0,0,1,1,1,2,2,3,3,4};
+        int[] arr3 = new int[] {1,2,4, 5, 6, 10};
+
+        int res0 = sol.removeDuplicates(arr1);
+	    int res1 = sol.removeDuplicates(arr2);
+        int res2 = sol.removeDuplicates(arr3);
+    }
+}
