c1bfd3b6a1fc894727a2b9794b928361019fb72a
chopdev <taras.plavin@gmail.com>
2018-12-10 23:54:16 +0200

    add more solutions for 395 longest substring

--- dynamic_programming/395_longest_substring_with_at_least_k_repeating_chars/Solution.java
+++ dynamic_programming/395_longest_substring_with_at_least_k_repeating_chars/Solution.java
@@ -65,7 +65,7 @@ public class Solution {
 
     // My dynamic programming solution
     // Time O(N^3), N*N - calls and each call makes N operations
-    // Space O(N^2)
+    // Space O(N^2), works fast enough, but get Memory Limit Exception
     public int longestSubstring2222(String s, int k) {
         if(s == null || s.length() < k) return 0;
         int[][] dp = new int[s.length()][s.length()];
@@ -79,24 +79,73 @@ public class Solution {
         if(hi - lo + 1 < k) return 0;
         if(dp[lo][hi] > -1) return dp[lo][hi];
 
-        HashMap<Character, Integer> counts = new HashMap<>();
-        for (int i = lo; i <=hi ; i++) {
-            char curr = s.charAt(i);
-            if(!counts.containsKey(curr)) counts.put(curr, 1);
-            else counts.put(curr, counts.get(curr) + 1);
-        }
+        int[] counts = new int[26];
+        for (int i = lo; i <= hi ; i++)
+            counts[s.charAt(i) - 'a'] += 1;
 
-        boolean valid = true;
-        for(int number : counts.values()) {
-            if(number < k) {
-                valid = false;
-                break;
-            }
-        }
+        int ind;
+        for (ind = lo; ind <= hi && counts[s.charAt(ind) - 'a'] >= k; ind++) { }
 
-        if(valid) dp[lo][hi] = hi - lo + 1;
+        if(ind == hi + 1) dp[lo][hi] = hi - lo + 1;
         else
             dp[lo][hi] = Math.max(longestSubstring(s, lo + 1, hi, k, dp), longestSubstring(s, lo, hi - 1, k, dp));
         return dp[lo][hi];
+    }
+
+    // Not mine, recursive solution
+    // not the quickest one, looking in leetcode
+    // https://leetcode.com/problems/longest-substring-with-at-least-k-repeating-characters/discuss/87736/C++-recursive-solution
+    public int longestSubstring3333(String s, int k) {
+        if(s == null || s.length() < k) return 0;
+
+        //  record counts of every character in a hashmap
+        int[] counts = new int[26];
+        for (int i = 0; i < s.length() ; i++)
+            counts[s.charAt(i) - 'a'] += 1;
+
+        // locate the first character that appear less than k times in the string. this character is
+        // not included in the result, and that separates the string into two parts
+        int ind;
+        for (ind = 0; ind < s.length() && counts[s.charAt(ind) - 'a'] >= k; ind++) { }
+
+        if(ind == s.length()) return s.length();
+
+        int left = longestSubstring3333(s.substring(0, ind), k);
+        int right = longestSubstring3333(s.substring(ind + 1, s.length()), k);
+
+        return Math.max(left, right);
+    }
+
+
+    // Not mine solution
+    // one of the quickest, looking in leetcode
+    // idea is similar to previous one, but they do not do substring
+    // and throw away ranges of chars that appear < k times
+    public int longestSubstring4444(String s, int k) {
+        return helper(s.toCharArray(), 0, s.length(), k);
+    }
+
+    private int helper(char c[], int left, int right, int k){
+
+        if(right - left < k) return 0;
+
+        char count[] = new char[26];
+        for(int i = left; i < right; i++)
+            count[c[i] - 'a']++;
+
+        for(int i = left; i < right; i++){
+
+            if(count[c[i] - 'a'] < k){
+
+                int j = i + 1;
+
+                while(j < right && count[c[j] - 'a'] < k) j++;
+
+                return Math.max(helper(c, left, i, k), helper(c, j, right, k));
+
+            }
+
+        }
+        return right - left;
     }
 }
