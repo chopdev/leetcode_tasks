befe59d9d689c9877ddc8cab5e1053d8e1587ffa
chopdev <taras.plavin@gmail.com>
2019-02-01 17:45:30 +0200

    add solution for 134 gas station

---
+++ math/134_gas_station/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    public static void main(String[] args){
+        Solution sol = new Solution();
+        int res = sol.canCompleteCircuit(new int[] {1, 2, 3, 4, 5}, new int[] {3, 4, 5, 1, 2});
+    }
+}
