c53656f97e8cd9187bb6f4abbec061ca6518ff0e
chopdev <taras.plavin@gmail.com>
2019-02-03 23:17:24 +0200

    add solution for 108 convert sorted array to bst

---
+++ tree/108_convert_sorted_array_to_binary_search_tree/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    public static void main(String[] args) {
+	    Solution sol = new Solution();
+	    TreeNode res = sol.sortedArrayToBST(new int[] {1, 2, 3, 4, 5,6,7,8,9});
+    }
+}
