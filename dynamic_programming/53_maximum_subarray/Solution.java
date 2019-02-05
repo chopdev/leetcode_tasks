6aeab1d3e869bcd2573addf959267889bde718bb
chopdev <taras.plavin@gmail.com>
2019-02-05 18:09:02 +0200

    minor fix

--- dynamic_programming/53_maximum_subarray/Solution.java
+++ dynamic_programming/53_maximum_subarray/Solution.java
@@ -28,8 +28,8 @@ public class Solution {
 
         int[] dp = new int[nums.length];
         dp[0] = nums[0];
-
-        int max = Integer.MIN_VALUE;
+        int max = dp[0];
+		
         for (int i = 1; i < nums.length; i++) {
             dp[i] = dp[i-1] > 0 ? dp[i-1] + nums[i] : nums[i];
             max = max > dp[i] ? max : dp[i];
