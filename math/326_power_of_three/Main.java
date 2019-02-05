38c1f7aff0579bd10fb0376f009e8b70ccbfcc5c
chopdev <taras.plavin@gmail.com>
2019-02-05 16:16:56 +0200

    add other solutions for 326 power of three

--- math/326_power_of_three/Main.java
+++ math/326_power_of_three/Main.java
@@ -3,5 +3,9 @@ public class Main {
     public static void main(String[] args) {
 	    Solution sol = new Solution();
         boolean res = sol.isPowerOfThree(27);
+
+
+        //System.out.println(0%3);
+        System.out.println(Integer.MAX_VALUE);
     }
 }
