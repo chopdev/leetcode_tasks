0859bef61e2d65e880f834cf6374e52f742dfb89
chopdev <taras.plavin@gmail.com>
2019-03-28 15:45:47 +0200

    84 largest rectangle in histograg

---
+++ array/84_largest_rectangle_in_histogram/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+	    Solution sol = new Solution();
+        sol.largestRectangleArea(new int [] {4, 4, 4});
+        sol.largestRectangleArea(new int [] {1, 3, 3, 3});
+        sol.largestRectangleArea(new int [] {4, 4, 4, 4, 6, 1, 1, 1, 1 ,1, 1, 1});
+        sol.largestRectangleArea(new int [] {1, 3, 1, 3});
+        sol.largestRectangleArea(new int [] {1, 1, 4, 4});
+    }
+}
