bf4bdd250bb0fe6bc3d30b2c0feecfd4d8d3f16a
chopdev <taras.plavin@gmail.com>
2019-03-30 21:24:41 +0200

    455 assign cookies

---
+++ greedy/455_assign_cookies/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+	    Solution sol = new Solution();
+	    int count1 = sol.findContentChildren(new int[] {1, 2, 3}, new int[] {1, 1});
+        int count2 = sol.findContentChildren(new int[] {1, 2}, new int[] {1, 2, 3});
+    }
+}
