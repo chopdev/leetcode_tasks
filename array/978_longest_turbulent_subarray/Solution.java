872f3ae49f7bc4864797ca6313ae46296f8cad70
chopdev <taras.plavin@gmail.com>
2019-04-06 14:42:46 +0300

    978 not mine solution

--- array/978_longest_turbulent_subarray/Solution.java
+++ array/978_longest_turbulent_subarray/Solution.java
@@ -63,4 +63,26 @@ public class Solution {
         }
         return max;
     }
+
+
+    // Not mine cool solution
+    // O(N) time with one pass
+    // https://leetcode.com/problems/longest-turbulent-subarray/discuss/221935/Java-O(N)-time-O(1)-space
+    public int maxTurbulenceSize2222(int[] A) {
+        int inc = 1, dec = 1, result = 1;
+        for (int i = 1; i < A.length; i++) {
+            if (A[i] < A[i - 1]) {
+                dec = inc + 1;
+                inc = 1;
+            } else if (A[i] > A[i - 1]) {
+                inc = dec + 1;
+                dec = 1;
+            } else {
+                inc = 1;
+                dec = 1;
+            }
+            result = Math.max(result, Math.max(dec, inc));
+        }
+        return result;
+    }
 }
