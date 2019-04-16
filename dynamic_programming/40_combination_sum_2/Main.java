25c7a07891757d071a8f14c3b9a10715ce1a6fad
chopdev <taras.plavin@gmail.com>
2019-04-16 18:31:37 +0300

    40 combination sum 2

---
+++ dynamic_programming/40_combination_sum_2/Main.java
@@ -0,0 +1,9 @@
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        List<List<Integer>> res = sol.combinationSum2(new int[] {10,1,2,7,6,1,5}, 8);
+    }
+}
