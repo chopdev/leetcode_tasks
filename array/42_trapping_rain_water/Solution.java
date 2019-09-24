2e52aaedad3b4994c5d5474b518105b25ed732aa
chopdev <taras.plavin@gmail.com>
2019-09-23 21:22:57 -0700

    1021 remove outermost parentheses

--- array/42_trapping_rain_water/Solution.java
+++ array/42_trapping_rain_water/Solution.java
@@ -64,4 +64,55 @@ public class Solution {
         }
         return ans;
     }
+	
+	// Not mine solution - good idea
+	// https://leetcode.com/problems/trapping-rain-water/discuss/17386/Sharing-my-Java-code%3A-O(n)-time-O(1)-space
+	public int trap3333(int[] A) {
+		if (A.length < 3) return 0;
+		
+		int ans = 0;
+		int l = 0, r = A.length - 1;
+		
+		// find the left and right edge which can hold water
+		while (l < r && A[l] <= A[l + 1]) l++;
+		while (l < r && A[r] <= A[r - 1]) r--;
+		
+		while (l < r) {
+			int left = A[l];
+			int right = A[r];
+			if (left <= right) {
+				// add volum until an edge larger than the left edge
+				while (l < r && left >= A[++l]) {
+					ans += left - A[l];
+				}
+			} else {
+				// add volum until an edge larger than the right volum
+				while (l < r && A[--r] <= right) {
+					ans += right - A[r];
+				}
+			}
+		}
+		return ans;
+	}
+	
+	public int trap4444(int[] A){
+		int a=0;
+		int b=A.length-1;
+		int max=0;
+		int leftmax=0;
+		int rightmax=0;
+		while(a<=b){
+			leftmax=Math.max(leftmax,A[a]);
+			rightmax=Math.max(rightmax,A[b]);
+			if(leftmax<rightmax){
+				max+=(leftmax-A[a]);       // leftmax is smaller than rightmax, so the (leftmax-A[a]) water can be stored
+				a++;
+			}
+			else{
+				max+=(rightmax-A[b]);
+				b--;
+			}
+		}
+		return max;
+	}
 }
