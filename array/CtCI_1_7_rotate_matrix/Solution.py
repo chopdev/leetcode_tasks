33519ce585dff86de599b6c5a445a0c1b23deb54
chopdev <taras.plavin@gmail.com>
2021-05-31 23:54:34 -0700

    add more fancy solutions for 1_7

--- array/CtCI_1_7_rotate_matrix/Solution.py
+++ array/CtCI_1_7_rotate_matrix/Solution.py
@@ -1,6 +1,7 @@
 #Rotate Matrix: Given an image represented by an NxN matrix, where each pixel in the image is 4
 # bytes, write a method to rotate the image by 90 degrees. Can you do this in place?
 
+# https://leetcode.com/problems/rotate-image/
 
 #
 #    1  2  3 4        1 2 3  
@@ -12,7 +13,7 @@
 # consider rotation of not first element in a row
 
 
-# O(N^2) solution
+# O(N^2) solution, N - one side lenght
 def rotate(matrix):
     n = len(matrix)
     for layer in range(0, n//2 + n%2):  # n//2 == int(n/2), it rounds to lower value
@@ -29,6 +30,36 @@ def swap(matrix, i, j, k, m):
 
 
 
+# not mine solution, but iteresting idea, much easier to implement without mistakes
+# https://leetcode.com/problems/rotate-image/solution/
+def rotate2222(matrix):
+    if matrix is None or len(matrix) != len(matrix[0]):
+        raise ValueError('invalid matrix')
+    transpose(matrix)
+    reflect(matrix)
+
+def transpose(matrix):
+    for i in range(len(matrix)):
+        for j in range(i+1, len(matrix)):
+            matrix[j][i], matrix[i][j] = matrix[i][j], matrix[j][i]
+
+def reflect(matrix):
+    N = len(matrix)
+    for row in range(N):
+        for j in range(N // 2):
+            matrix[row][j], matrix[row][N - 1 - j] = matrix[row][N - 1 - j], matrix[row][j]
+
+
+# https://leetcode.com/problems/rotate-image/discuss/18884/Seven-Short-Solutions-(1-to-7-lines)
+# Nice solution, will work with N*M matrix, BUT not completely in place
+def rotate3333(matrix):
+    matrix[:] = map(list, zip(*matrix[::-1]))
+
+# https://leetcode.com/problems/rotate-image/discuss/18884/Seven-Short-Solutions-(1-to-7-lines)
+# similar to rotate2222 but in different order
+def rotate4444(matrix):
+    matrix.reverse() # reflect rows firstly
+    transpose(matrix)
 
 
 ###########################################
@@ -41,7 +72,9 @@ def printMatrix(matrix):
     print()
 
 printMatrix(matrix)
-rotate(matrix)
+rotate4444(matrix)
+#print(list(zip(*matrix[::-1])))
+
 printMatrix(matrix)
 
 
