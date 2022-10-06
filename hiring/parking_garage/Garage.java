50887b3607396a1a73e29983428743bb9baaf245
chopdev <taras.plavin@gmail.com>
2022-10-06 00:15:56 -0700

    add parking garage interview question

---
+++ hiring/parking_garage/Garage.java
@@ -0,0 +1,56 @@
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+/**
+ * Given a parking garage with two functions, implement enter and exit functions
+ * Garage size: 5 floors, 20 spots on the floor
+ *
+ *
+ * Good qustions to ask:
+ * - can we have different car sizes?
+ * - should we care about concurrency?
+ * */
+public class Garage {
+
+    class Spot {
+        String id;
+        int floor;
+
+        public Spot(String id, int floor) {
+          this.id = id;
+          this.floor = floor;
+        }
+    }
+
+    Deque<Spot> allSpots = new LinkedList<>();
+    Map<Ticket, Spot> occupied = new HashMap<>();
+
+    public Garage(Map<Integer, List<String>> floorToSpots) {
+        for (Integer floor : floorToSpots.keySet()) {
+            for (String spot : floorToSpots.get(floor)) {
+                allSpots.add(new Spot(spot, floor));
+            }
+        }
+    }
+
+    public Ticket enter() {
+        if (allSpots.isEmpty()) return null; // or exception
+
+        Spot spotToTake = allSpots.removeFirst();
+        Ticket ticket = new Ticket(UUID.randomUUID().toString(), spotToTake.floor);
+        occupied.put(ticket, spotToTake);
+        return ticket;
+    }
+
+    public void exit(Ticket ticket) {
+        if (!occupied.containsKey(ticket)) return; // or exception
+
+        Spot freeSpot = occupied.remove(ticket);
+        allSpots.offer(freeSpot);
+    }
+
+}
---
+++ hiring/parking_garage/Ticket.java
@@ -0,0 +1,9 @@
+public class Ticket {
+    public String id; // ticket id
+    public int floor; // floor level
+
+    public Ticket(String id, int floor) {
+        this.id = id;
+        this.floor = floor;
+    }
+}
