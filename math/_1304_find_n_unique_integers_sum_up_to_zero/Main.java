054f5b4d4d29839d2d1da7fa24c4deefe9a657d2
chopdev <taras.plavin@gmail.com>
2022-12-04 21:52:49 -0800

    1304 find n unique integers

---
+++ math/_1304_find_n_unique_integers_sum_up_to_zero/Main.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(Arrays.toString(sol.sumZero(0)));
+        System.out.println(Arrays.toString(sol.sumZero(1)));
+        System.out.println(Arrays.toString(sol.sumZero(2)));
+        System.out.println(Arrays.toString(sol.sumZero(3)));
+        System.out.println(Arrays.toString(sol.sumZero(4)));
+    }
+}
