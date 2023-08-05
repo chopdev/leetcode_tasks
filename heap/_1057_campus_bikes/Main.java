c5f3d8164980f5711fabf0e0244e1e40af3997c0
chopdev <taras.plavin@gmail.com>
2023-08-04 21:39:44 -0700

    _1057_campus_bikes

---
+++ heap/_1057_campus_bikes/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        sol.assignBikes(new int[][]{{0, 0}, {1, 1}, {2, 0}}, new int[][] {{1, 0}, {2, 2}, {2, 1}});
+    }
+}
