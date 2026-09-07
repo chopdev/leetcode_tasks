eb9e57d5bbc04037a7633890288bd78f2c7638c9
chopdev <taras.plavin@gmail.com>
2026-09-06 23:20:39 -0700

    103_binary_tree_zigzag_level_order_traversal

--- tree/103_binary_tree_zigzag_level_order_traversal/Solution.java
+++ tree/103_binary_tree_zigzag_level_order_traversal/Solution.java
@@ -1,78 +1,43 @@
+/*
+103. Binary Tree Zigzag Level Order Traversal
+https://leetcode.com/problems/binary-tree-zigzag-level-order-traversal/
+
+Given the root of a binary tree, return the values level by level, alternating
+between left-to-right and right-to-left order on successive levels.
+*/
+
+import java.util.ArrayDeque;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.LinkedList;
+import java.util.Deque;
 import java.util.List;
+import java.util.Queue;
 
-/**
- 103. Binary Tree Zigzag Level Order Traversal
- https://leetcode.com/problems/binary-tree-zigzag-level-order-traversal/
-
- Given a binary tree, return the zigzag level order traversal of its nodes' values. (ie, from left to right, then right to left for the next level and alternate between).
-
- For example:
- Given binary tree [3,9,20,null,null,15,7],
- 3
- / \
- 9  20
- /  \
- 15   7
- return its zigzag level order traversal as:
- [
- [3],
- [20,9],
- [15,7]
- ]
-
-* */
-public class Solution {
-    // My solution
-    // Use level order traversal + reverse each odd list in result list
-    // O(N) time and space
+class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        if (root == null) return new ArrayList<>();
+
         List<List<Integer>> res = new ArrayList<>();
-        traversal(root, 0, res);
+        Queue<TreeNode> q = new ArrayDeque<>();
+        q.add(root);
+        boolean inOrder = true;
 
-        for (int i = 1; i < res.size(); i+= 2) {
-            Collections.reverse(res.get(i));
+        while (!q.isEmpty()) {
+            int size = q.size();
+            Deque<Integer> layer = new ArrayDeque<>();
+
+            for (int i = 0; i < size; i++) {
+                TreeNode node = q.poll();
+                if (inOrder) layer.addLast(node.val);
+                else layer.addFirst(node.val);
+
+                if (node.left != null) q.add(node.left);
+                if (node.right != null) q.add(node.right);
+            }
+
+            res.add(new ArrayList<>(layer));
+            inOrder = !inOrder;
         }
+
         return res;
-    }
-
-    private void traversal(TreeNode curr, int level, List<List<Integer>> res) {
-        if(curr == null) return;
-
-        if(res.size() <= level) res.add(new ArrayList<>());
-        res.get(level).add(curr.val);
-        traversal(curr.left, level + 1, res);
-        traversal(curr.right, level + 1, res);
-    }
-
-
-    // Not mine
-    // Cool solution, uses linked list in order to insert to beginning when level is odd
-    // O(N) time and space
-    public List<List<Integer>> zigzagLevelOrder2222(TreeNode root)
-    {
-        List<List<Integer>> sol = new ArrayList<>();
-        travel(root, sol, 0);
-        return sol;
-    }
-
-    private void travel(TreeNode curr, List<List<Integer>> sol, int level)
-    {
-        if(curr == null) return;
-
-        if(sol.size() <= level)
-        {
-            List<Integer> newLevel = new LinkedList<>();
-            sol.add(newLevel);
-        }
-
-        List<Integer> collection  = sol.get(level);
-        if(level % 2 == 0) collection.add(curr.val);
-        else collection.add(0, curr.val);
-
-        travel(curr.left, sol, level + 1);
-        travel(curr.right, sol, level + 1);
     }
 }
