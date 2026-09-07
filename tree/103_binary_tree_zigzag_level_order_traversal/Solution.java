a8864f5270f03a1b8e49c5e82443d27626c580c8
chopdev <taras.plavin@gmail.com>
2026-09-06 23:24:50 -0700

    103_binary_tree_zigzag_level_order_traversal

--- tree/103_binary_tree_zigzag_level_order_traversal/Solution.java
+++ tree/103_binary_tree_zigzag_level_order_traversal/Solution.java
@@ -8,6 +8,7 @@ between left-to-right and right-to-left order on successive levels.
 
 import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Deque;
 import java.util.List;
 import java.util.Queue;
@@ -39,5 +40,27 @@ class Solution {
         }
 
         return res;
+    }
+
+    // Solution 2: recursive DFS followed by reversing every odd-numbered level.
+    public List<List<Integer>> zigzagLevelOrder2(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        traversal(root, 0, res);
+
+        for (int i = 1; i < res.size(); i += 2) {
+            Collections.reverse(res.get(i));
+        }
+
+        return res;
+    }
+
+    private void traversal(TreeNode curr, int level, List<List<Integer>> res) {
+        if (curr == null) return;
+
+        if (res.size() <= level) res.add(new ArrayList<>());
+        res.get(level).add(curr.val);
+
+        traversal(curr.left, level + 1, res);
+        traversal(curr.right, level + 1, res);
     }
 }
