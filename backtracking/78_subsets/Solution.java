f258476523bb985f46f985f6b88ce4274b751a91
chopdev <taras.plavin@gmail.com>
2026-09-16 22:03:58 -0700

    78_subsets

--- backtracking/78_subsets/Solution.java
+++ backtracking/78_subsets/Solution.java
@@ -1,7 +1,3 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 /*
 78. Subsets
 https://leetcode.com/problems/subsets/
@@ -29,12 +25,45 @@ Output:
 good explanation
 https://www.youtube.com/watch?v=bGC2fNALbNU
 * */
-public class Solution {
-    // Not mine solution
-    // O(2^N) time as we would have 2^N subsets - each time we take or do nat take an element,
-    // O(2N)= O(N) space - tempList + N depth of recursion
-    // https://leetcode.com/problems/subsets/discuss/27281/A-general-approach-to-backtracking-questions-in-Java-(Subsets-Permutations-Combination-Sum-Palindrome-Partitioning)
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class Solution {
+    /*
+     [], [1], [1, 2], [1, 2, 3], [1, 3], [2], [2, 3], [3]
+
+     For every element there are two states: include it or don't include it.
+     Therefore, there are 2^N subsets.
+     Time: O(N * 2^N), accounting for copying the subsets.
+     Auxiliary space: O(N) for the current subset and recursion stack.
+     Output space: O(N * 2^N).
+    */
     public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        backtrack(nums, 0, new ArrayList<>(), res);
+        return res;
+    }
+
+    private void backtrack(
+        int[] nums,
+        int start,
+        List<Integer> curr,
+        List<List<Integer>> res
+    ) {
+        res.add(new ArrayList<>(curr));
+
+        for (int i = start; i < nums.length; i++) {
+            curr.add(nums[i]);
+            backtrack(nums, i + 1, curr, res);
+            curr.remove(curr.size() - 1);
+        }
+    }
+
+    // Solution 2: previous implementation (not mine), sorts the input first.
+    // Time: O(N * 2^N); auxiliary space: O(N); output space: O(N * 2^N).
+    // https://leetcode.com/problems/subsets/discuss/27281/A-general-approach-to-backtracking-questions-in-Java-(Subsets-Permutations-Combination-Sum-Palindrome-Partitioning)
+    public List<List<Integer>> subsets2(int[] nums) {
         List<List<Integer>> list = new ArrayList<>();
         Arrays.sort(nums);
         backtrack(list, new ArrayList<>(), nums, 0);
