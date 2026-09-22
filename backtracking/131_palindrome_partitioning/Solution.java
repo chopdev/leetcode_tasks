aa63bfffe14d182280a641135f2788f04a607b41
chopdev <taras.plavin@gmail.com>
2026-09-22 00:29:26 -0700

    131_palindrome_partitioning

--- backtracking/131_palindrome_partitioning/Solution.java
+++ backtracking/131_palindrome_partitioning/Solution.java
@@ -1,9 +1,6 @@
-import java.util.ArrayList;
-import java.util.List;
-
 /*
-https://leetcode.com/problems/palindrome-partitioning/
 131. Palindrome Partitioning
+https://leetcode.com/problems/palindrome-partitioning/
 
 Given a string s, partition s such that every substring of the partition is a palindrome.
 
@@ -19,16 +16,88 @@ Output:
 ]
 
 * */
-public class Solution {
+import java.util.ArrayList;
+import java.util.List;
 
-    // Not mine solution
+class Solution {
+
+    public List<List<String>> partition(String s) {
+        List<List<String>> res = new ArrayList<>();
+        backtrack(s, 0, new StringBuilder(), new ArrayList<>(), res);
+        return res;
+    }
+
+    /**
+     * At each character, either extend curr hoping to form a palindrome,
+     * or start curr again if its previous contents form a palindrome.
+     *
+     * Let S be the length of s.
+     * Time: O(S * 2^S), with at most two choices per character and O(S)
+     * work per recursive call for palindrome checks, copies, and restoration.
+     * Auxiliary space: O(S) for curr, partitions, and the recursion stack.
+     * Output space: O(S * 2^S) in the worst case.
+     */
+    private void backtrack(String s,
+        int i,
+        StringBuilder curr,
+        List<String> partitions,
+        List<List<String>> res) {
+
+        if (i == s.length()) {
+            if (isPalindrome(curr)) {
+                partitions.add(curr.toString());
+                res.add(new ArrayList<>(partitions));
+                partitions.remove(partitions.size() - 1);
+            }
+            return;
+        }
+
+        curr.append(s.charAt(i));
+        backtrack(s, i + 1, curr, partitions, res);
+        curr.deleteCharAt(curr.length() - 1);
+
+        if (isPalindrome(curr)) {
+            String temp = curr.toString();
+
+            partitions.add(temp);
+            curr.setLength(0);
+
+            curr.append(s.charAt(i));
+            backtrack(s, i + 1, curr, partitions, res);
+
+            partitions.remove(partitions.size() - 1);
+            curr.setLength(0);
+            curr.append(temp);
+        }
+    }
+
+    private boolean isPalindrome(StringBuilder sb) {
+        if (sb == null || sb.length() == 0) return false;
+
+        String curr = sb.toString();
+        int len = curr.length();
+        if (len == 1) return true;
+
+        if (len % 2 == 1) { // odd number, we need to split by middle char
+            String st1 = curr.substring(0, len / 2);
+            String st2 = new StringBuilder(curr.substring(len / 2 + 1)).reverse().toString();
+
+            return st1.equals(st2);
+        } else {
+            String st1 = curr.substring(0, len / 2);
+            String st2 = new StringBuilder(curr.substring(len / 2)).reverse().toString();
+            return st1.equals(st2);
+        }
+    }
+
+    // Solution 2: previous implementation (not mine).
     // https://leetcode.com/problems/palindrome-partitioning/discuss/41963/Java%3A-Backtracking-solution.
     /* Time complexity: O(n*(2^n))
     For a string with length n, there will be (n - 1) intervals between chars.
     For every interval, we can cut it or not cut it, so there will be 2^(n - 1) ways to partition the string.
     For every partition way, we need to check if it is palindrome, which is O(n).
     So the time complexity is O(n*(2^n)) */
-    public List<List<String>> partition(String s) {
+    public List<List<String>> partition2(String s) {
         List<List<String>> res = new ArrayList<>();
         dfs(s, 0, new ArrayList<>(), res);
         return res;
